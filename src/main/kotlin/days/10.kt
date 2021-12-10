5e6692f3be6f4ea39c56b9d5ab7e117acfc81250
n.e.hullegien@student.tudelft.nl <n.e.hullegien@student.tudelft.nl>
2021-12-10 10:41:40 +0100

    10

--- src/main/kotlin/days/10.kt
+++ src/main/kotlin/days/10.kt
@@ -1,18 +1,136 @@
 package days
 
 import java.io.File
+import java.lang.Math.floor
+import java.util.*
 
 fun main() {
-    run10a()
+//    run10a()
     run10b()
 }
 
 fun run10a() {
-    val list = File("src/main/resources/10/10-test.txt").readLines()
+    val list = File("src/main/resources/10/10-input.txt").readLines()
         .map { line -> line.toCharArray().toList() }
-    println(list)
+    val chunkStack = Stack<Char>()
+    var score = 0
+    for (line in list) {
+        chunkStack.clear()
+        for (char in line) {
+            when (char) {
+                '[' -> chunkStack.push(']')
+                '(' -> chunkStack.push(')')
+                '<' -> chunkStack.push('>')
+                '{' -> chunkStack.push('}')
+                ']' -> {
+                    if (chunkStack.peek().equals(']')) {
+                        chunkStack.pop()
+                    } else {
+                        println("EXPECTED ] but was $char in line $line")
+                        score += 57
+                        break
+                    }
+                }
+                ')' -> {
+                    if (chunkStack.peek().equals(')')) {
+                        chunkStack.pop()
+                    } else {
+                        println("EXPECTED ) but was $char in line $line")
+                        score += 3
+                        break
+                    }
+                }
+                '>' -> {
+                    if (chunkStack.peek().equals('>')) {
+                        chunkStack.pop()
+                    } else {
+                        println("EXPECTED > but was $char in line $line")
+                        score += 25137
+                        break
+                    }
+                }
+                '}' -> {
+                    if (chunkStack.peek().equals('}')) {
+                        chunkStack.pop()
+                    } else {
+                        println("EXPECTED } but was $char in line $line")
+                        score += 1197
+                        break
+                    }
+                }
+            }
+        }
+    }
+    println("10a: $score")
 }
 
 fun run10b() {
-    TODO("Not yet implemented")
+    val list = File("src/main/resources/10/10-input.txt").readLines()
+        .map { line -> line.toCharArray().toList() }
+    val chunkStack = Stack<Char>()
+    val scoreList = mutableListOf<Long>()
+    for (line in list) {
+        var isCorrupted = false
+        chunkStack.clear()
+        for (char in line) {
+            when (char) {
+                '[' -> chunkStack.push(']')
+                '(' -> chunkStack.push(')')
+                '<' -> chunkStack.push('>')
+                '{' -> chunkStack.push('}')
+                ']' -> {
+                    if (chunkStack.peek().equals(']')) {
+                        chunkStack.pop()
+                    } else {
+                        println("EXPECTED ] but was $char in line $line")
+                        isCorrupted = true
+                        break
+                    }
+                }
+                ')' -> {
+                    if (chunkStack.peek().equals(')')) {
+                        chunkStack.pop()
+                    } else {
+                        println("EXPECTED ) but was $char in line $line")
+                        isCorrupted = true
+                        break
+                    }
+                }
+                '>' -> {
+                    if (chunkStack.peek().equals('>')) {
+                        chunkStack.pop()
+                    } else {
+                        println("EXPECTED > but was $char in line $line")
+                        isCorrupted = true
+                        break
+                    }
+                }
+                '}' -> {
+                    if (chunkStack.peek().equals('}')) {
+                        chunkStack.pop()
+                    } else {
+                        println("EXPECTED } but was $char in line $line")
+                        isCorrupted = true
+                        break
+                    }
+                }
+            }
+        }
+        if (!isCorrupted) {
+            println("INCOMPLETE LINE $line with chunkstack $chunkStack")
+            var score = 0L
+            while (!chunkStack.isEmpty()) {
+                score *= 5L
+                when(chunkStack.pop()) {
+                    ')' -> score += 1L
+                    ']' -> score += 2L
+                    '}' -> score += 3L
+                    '>' -> score += 4L
+                }
+            }
+            scoreList.add(score)
+        }
+    }
+    scoreList.sort()
+    println("10b: ${scoreList[scoreList.size/2]}")
 }
