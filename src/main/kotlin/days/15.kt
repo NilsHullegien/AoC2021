2aec31230417d08b3f885a66b4cc5b9ef89d432d
n.e.hullegien@student.tudelft.nl <n.e.hullegien@student.tudelft.nl>
2021-12-15 07:20:45 +0100

    backup15

--- src/main/kotlin/days/15.kt
+++ src/main/kotlin/days/15.kt
@@ -7,7 +7,7 @@ fun main() {
     run15b()
 }
 
-class Coord(val riskValue: Int, var minRiskToReach: Int) {
+class Coord(var riskValue: Int, var minRiskToReach: Int) {
     override fun toString(): String {
         return "(riskValue=$riskValue, minRiskToReach=$minRiskToReach)"
     }
@@ -18,78 +18,65 @@ fun run15a() {
         .map { line -> line.toCharArray().map { el -> Coord(Character.getNumericValue(el), Int.MAX_VALUE) }.toList() }
     val startingPos = Pair(0,0)
     val endPoint = Pair(grid.size-1, grid.size-1)
-    val totalRisk = 0
-    findPath(grid, startingPos, endPoint, totalRisk, mutableListOf())
-    println("15a: ${grid[grid.size - 1][grid.size - 1].minRiskToReach + grid[grid.size - 1][grid.size - 1].riskValue}")
-}
-
-fun findPath(grid: List<List<Coord>>, currentPos: Pair<Int, Int>, endPoint: Pair<Int, Int>, totalRisk: Int, path: MutableList<Pair<Int, Int>>) {
-    println("CURRENT POS: $currentPos, ${grid[currentPos.first][currentPos.second]}")
-    if (currentPos == endPoint) {
-        return
-    }
-    if (currentPos.first >= 1) {
-        println("COORD LEFT, path $path")
-        val newPos = Pair(currentPos.first-1, currentPos.second)
-        if (path.contains(newPos)) {
-            println("NEWPOS $newPos ALREADY VISITED!")
-        } else {
-            val newCoord = grid[currentPos.first-1][currentPos.second]
-            if (totalRisk <= newCoord.minRiskToReach) {
-                newCoord.minRiskToReach = totalRisk
-                val newPath = path.toMutableList()
-                newPath.add(newPos)
-                findPath(grid, Pair(currentPos.first - 1, currentPos.second), endPoint, totalRisk + grid[currentPos.first][currentPos.second].riskValue, newPath)
-            }
-        }
-    }
-    if (currentPos.first < endPoint.first) {
-        println("COORD RIGHT, path $path")
-        val newPos = Pair(currentPos.first+1, currentPos.second)
-        if (path.contains(newPos)) {
-            println("NEWPOS $newPos ALREADY VISITED!")
-        } else {
-            val newCoord = grid[currentPos.first+1][currentPos.second]
-            if (totalRisk <= newCoord.minRiskToReach) {
-                newCoord.minRiskToReach = totalRisk
-                val newPath = path.toMutableList()
-                newPath.add(newPos)
-                findPath(grid, Pair(currentPos.first + 1, currentPos.second), endPoint, totalRisk + grid[currentPos.first][currentPos.second].riskValue, newPath)
-            }
-        }
-    }
-    if (currentPos.second >= 1) {
-        println("COORD TOP, path $path")
-        val newPos = Pair(currentPos.first, currentPos.second - 1)
-        if (path.contains(newPos)) {
-            println("NEWPOS $newPos ALREADY VISITED!")
-        } else {
-            val newCoord = grid[currentPos.first][currentPos.second-1]
-            if (totalRisk <= newCoord.minRiskToReach) {
-                newCoord.minRiskToReach = totalRisk
-                val newPath = path.toMutableList()
-                newPath.add(newPos)
-                findPath(grid, Pair(currentPos.first, currentPos.second-1), endPoint, totalRisk + grid[currentPos.first][currentPos.second].riskValue, newPath)
-            }
-        }
-    }
-    if (currentPos.second < endPoint.second) {
-        println("COORD BOTTOM, path $path")
-        val newPos = Pair(currentPos.first, currentPos.second+1)
-        if (path.contains(newPos)) {
-            println("NEWPOS $newPos ALREADY VISITED!")
-        } else {
-            val newCoord = grid[currentPos.first][currentPos.second+1]
-            if (totalRisk <= newCoord.minRiskToReach) {
-                newCoord.minRiskToReach = totalRisk
-                val newPath = path.toMutableList()
-                newPath.add(newPos)
-                findPath(grid, Pair(currentPos.first, currentPos.second+1), endPoint, totalRisk + grid[currentPos.first][currentPos.second].riskValue, newPath)
-            }
-        }
-    }
+    calculateEndRiskValues(grid, startingPos, endPoint)
+    println("15a: ${grid[grid.size - 1][grid.size - 1].minRiskToReach}")
 }
 
 fun run15b() {
+    val readGrid = File("src/main/resources/15/15-input.txt").readLines()
+        .map { line -> line.toCharArray().map { el -> Character.getNumericValue(el) }.toMutableList() }.toMutableList()
+    val megaGrid = mutableListOf<MutableList<Coord>>()
+    for (xStep in 0..4) {
+        for (x in 0 until readGrid.size) {
+            val list = mutableListOf<Coord>()
+            for (yStep in -1..3) {
+                for (y in 0 until readGrid.size) {
+                    list.add(Coord(((readGrid[x][y] + (yStep + xStep) ) % 9 + 1).coerceAtLeast(1), Int.MAX_VALUE))
+                }
+            }
+            megaGrid.add(list)
+        }
+    }
 
+    val startingPos = Pair(0,0)
+    val endPoint = Pair(megaGrid.size-1, megaGrid.size-1)
+    calculateEndRiskValues(megaGrid, startingPos, endPoint)
+    println("15b: ${megaGrid[megaGrid.size - 1][megaGrid.size - 1].minRiskToReach}")
+}
+
+fun calculateEndRiskValues(grid: List<List<Coord>>, startingPos: Pair<Int, Int>, endPoint: Pair<Int, Int>) {
+    val pointsToCheck = mutableListOf(startingPos)
+    grid[0][0].minRiskToReach = 0
+    while (pointsToCheck.any { it != endPoint }) {
+        val currentPos = pointsToCheck.removeAt(0)
+        val currentPosRiskToReach = grid[currentPos.first][currentPos.second].minRiskToReach
+        if (currentPos.first > 0) {
+            val newCoord = grid[currentPos.first - 1][currentPos.second]
+            if (newCoord.riskValue + currentPosRiskToReach < newCoord.minRiskToReach) {
+                newCoord.minRiskToReach = newCoord.riskValue + currentPosRiskToReach
+                pointsToCheck.add(Pair(currentPos.first - 1, currentPos.second))
+            }
+        }
+        if (currentPos.first < grid.size - 1) {
+            val newCoord = grid[currentPos.first + 1][currentPos.second]
+            if (newCoord.riskValue + currentPosRiskToReach < newCoord.minRiskToReach) {
+                newCoord.minRiskToReach = newCoord.riskValue + currentPosRiskToReach
+                pointsToCheck.add(Pair(currentPos.first + 1, currentPos.second))
+            }
+        }
+        if (currentPos.second > 0) {
+            val newCoord = grid[currentPos.first][currentPos.second - 1]
+            if (newCoord.riskValue + currentPosRiskToReach < newCoord.minRiskToReach) {
+                newCoord.minRiskToReach = newCoord.riskValue + currentPosRiskToReach
+                pointsToCheck.add(Pair(currentPos.first, currentPos.second - 1))
+            }
+        }
+        if (currentPos.second < grid.size - 1) {
+            val newCoord = grid[currentPos.first][currentPos.second + 1]
+            if (newCoord.riskValue + currentPosRiskToReach < newCoord.minRiskToReach) {
+                newCoord.minRiskToReach = newCoord.riskValue + currentPosRiskToReach
+                pointsToCheck.add(Pair(currentPos.first, currentPos.second + 1))
+            }
+        }
+    }
 }
